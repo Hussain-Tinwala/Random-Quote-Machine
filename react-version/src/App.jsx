3bec211a6a929fe928bb55830cc2ecba084bf401
Hussain Tinwala <coder.hussaintin@gmail.com>
2025-02-21 09:58:23 +0530

    Add drag and animations using motion

--- react-version/src/App.jsx
+++ react-version/src/App.jsx
@@ -11,12 +11,31 @@ function App() {
   return (
     <>
       <motion.div
+        drag
+        whileDrag={{
+          scale: 0.8
+        }}
+        dragConstraints={{
+          top: 0,
+          left: 0,
+          right:0,
+          bottom:50
+          
+        }}
         initial={{ opacity: 0, scale: 0.5 }}
-        animate={{ opacity: 1, scale: 1 }}
+        animate={{ 
+          opacity: 1, 
+          scale: 1,
+          // rotate:360
+          // x: 50,
+          // y:50
+         }}
         transition={{
             duration: 0.8,
             delay: 0.5,
             ease: [0, 0.71, 0.2, 1.01],
+            // repeat: 3,
+            // ease: 'anticipate'
         }}
         >
           <QuoteMachine />
